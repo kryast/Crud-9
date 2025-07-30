f812bdd17f9c22ca5eb3fc484d59899010e69767
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-07-30 15:01:22 +0700

    Create Article Models

---
+++ models/article.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Article struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	Author    string    `json:"author"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
